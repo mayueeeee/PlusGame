43820378879be54b29d4129b756552f70bb08713
Sarunyu Chankong <sarunyu.you@gmail.com>
2018-05-22 06:36:05 +0700

    Add status logic

--- display/src/App.js
+++ display/src/App.js
@@ -4,12 +4,13 @@ import styled from 'styled-components'
 import io from 'socket.io-client'
 import PlayerNumber from './Components/Player'
 import BigNumber, {SmallNumber, MaxNumber} from './Components/Number'
+import {LosePage, InitPage} from './Components/Pages'
+
 const FullViewContainer = styled(Container)`
   height: 100vh;  
   background: #0f0c29;  /* fallback for old browsers */
   background: -webkit-linear-gradient(to right, #24243e, #302b63, #0f0c29);  /* Chrome 10-25, Safari 5.1-6 */
   background: linear-gradient(to right, #24243e, #302b63, #0f0c29); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
   padding: 5vh 0 30px 0 ;
 `
 const LogoTXT = styled.h1 `
@@ -24,19 +25,21 @@ class App extends Component {
       current_number: 0,
       max_number: 0,
       plus_number: 0,
-      player_count:0,
-      current_player:0
+      player_count: 0,
+      current_player: 0,
+      isLose: false,
+      isStart: false
     };
 
   }
   componentDidMount() {
     const socket = io('http://localhost:8001');
-    
+
     socket.on('current_number', (data) => {
       console.log(`current_number: ${data.current_number}`)
       this.setState({current_number: data.current_number})
     })
-    
+
     socket.on('max_number', (data) => {
       console.log(`max_number: ${data.max_number}`)
       this.setState({max_number: data.max_number})
@@ -57,48 +60,95 @@ class App extends Component {
       this.setState({current_player: data.current_player})
     })
 
-    
+    // Status
+    socket.on('start', (data) => {
+      console.log(`Start!`)
+      this.setState({isStart: true})
+    })
+
+    socket.on('lose', (data) => {
+      console.log(`Lose!`)
+      this.setState({isLose: true})
+    })
+
+    socket.on('restart', (data) => {
+      console.log(`Restart!`)
+      this.setState({
+        current_number: 0,
+        max_number: 0,
+        plus_number: 0,
+        player_count: 0,
+        current_player: 0,
+        isLose: false,
+        isStart: false
+      })
+    })
 
   }
   render() {
-    return (
-      <FullViewContainer fluid={true} className="text-center">
-        <Row>
-          <Col xs={3}>
-            {/* <LogoTXT>eeeee</LogoTXT> */}
-            <PlayerNumber number={3} title="Player"/>
-          </Col>
-          <Col xs={6}>
-            <LogoTXT>Fucking Plus Game</LogoTXT>
-          </Col>
-          <Col xs={3}>
-            <MaxNumber number={this.state.max_number} title="Max Jaaaaa"/>
-          </Col>
-        </Row>
-        {/* <Row>
+    if (!this.state.isStart) {
+      return (
+        <div className="text-center">
+          <InitPage
+            maxPlayer={this.state.player_count}
+            maxNumber={this.state.max_number}/>
+        </div>
+      )
+    } else if (this.state.isLose) {
+      return (
+        <div className="text-center">
+          <LosePage player={this.state.current_player}/>
+        </div>
+      )
+    } else {
+      return (
+        <div className="text-center">
+          {/* <LosePage player={this.state.current_player} /> */}
+
+          <FullViewContainer fluid={true}>
+
+            <Row>
+              <Col xs={3}>
+                {/* <LogoTXT>eeeee</LogoTXT> */}
+                <PlayerNumber number={this.state.player_count} current_player ={this.state.current_player} title="Player"/>
+              </Col>
+              <Col xs={6}>
+                <LogoTXT>Count Game Yay!</LogoTXT>
+              </Col>
+              <Col xs={3}>
+                <MaxNumber number={this.state.max_number} title="Max Jaaaaa"/>
+              </Col>
+            </Row>
+            {/* <Row>
           <Col>
             <SmallNumber>200</SmallNumber>
           </Col>
         </Row> */}
-        <Row>
-          <Col>
-            <BigNumber>{this.state.current_number}</BigNumber>
-          </Col>
-          <Col md={1} className="d-flex h-100">
-            <div className="justify-content-center align-self-center">
-              <i
-                className="fas fa-plus fa-10x justify-content-center align-self-center fa-spin"></i>
-            </div>
+            <Row>
+              <Col>
+                <h2>Total</h2>
+                <BigNumber>{this.state.current_number}</BigNumber>
+              </Col>
+              <Col md={1} className="d-flex h-100">
+                <div className="justify-content-center align-self-center">
+                  <i
+                    className="fas fa-plus fa-10x justify-content-center align-self-center fa-spin"></i>
+                </div>
 
-          </Col>
-          <Col>
-            <BigNumber>{this.state.plus_number}</BigNumber>
-          </Col>
-        </Row>
+              </Col>
+              <Col>
+                <BigNumber>{this.state.plus_number}</BigNumber>
+              </Col>
+            </Row>
 
-      </FullViewContainer>
+          </FullViewContainer>
 
-    );
+        </div>
+
+      );
+
+    }
+
   }
 }
 
---
+++ display/src/Components/Pages.js
@@ -0,0 +1,107 @@
+import React, {Component} from 'react';
+import {Row, Col} from 'reactstrap'
+import styled from 'styled-components'
+import {LogoTXT, FullHeightContainer, SectionWarpper} from './Utils'
+import {MaxNumber} from './Number'
+import ReactPlayer from 'react-player'
+
+const PaneWarpper = styled.div `
+  width:100%;
+  height:100vh;  
+  background: -webkit-linear-gradient(to right, #4b134f, #c94b4b);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #4b134f, #c94b4b); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+  z-index: 2;
+  position: absolute;
+  /* margin-top: 22vh; */
+
+
+`
+const CenterCol = styled(Col)`
+  margin-top: auto;
+  margin-bottom: auto;
+`
+const FullRow = styled(Row)`
+  height:100%;
+
+`
+const LoseText = styled.h1 `
+  font-size:200px;
+  font-family: 'Noto Sans Thai UI', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  color: #fff;
+`
+
+const LoseSubText = styled.h2 `
+  /* font-size:100px; */
+  font-family: 'Noto Sans Thai UI','Laila', serif;
+  color: #fff;
+`
+const LoseMeme = styled.img `
+  height: 90vh;
+  margin-top: 10vh;
+  margin-left: 1vw;
+`
+export const StyleText = styled.h2 `
+  margin-top: 7vh;
+  font-size: 40px;
+  color: #fff;
+  font-family: 'Cabin Sketch', cursive;
+`
+
+// const LogoTXT = styled.h1 `   font-size:100px;   color: #fff;   font-family:
+// 'Pacifico', cursive; `
+
+export class LosePage extends Component {
+  render() {
+    return (
+      <PaneWarpper>
+        <FullRow>
+          <Col xs={2}><LoseMeme src="assets/lose_porr.png"/></Col>
+
+          <Col className="my-auto">
+            <LoseText>แตก1 สวยพี่สวย</LoseText>
+            <LoseSubText>~Player{this.props.player}, You Noob Na Jaaa Eiei~</LoseSubText>
+            <LoseSubText >กดปุ่มเพื่อเริ่มเล่นใหม่</LoseSubText >
+          </Col>
+        </FullRow>
+        <ReactPlayer
+          url='https://www.youtube.com/watch?v=hCC6ZhRS7sY'
+          width='0%'
+          height='0%'
+          loop
+          playing/>
+
+      </PaneWarpper>
+    );
+  }
+}
+
+export class InitPage extends Component {
+  render() {
+    return (
+
+      <FullHeightContainer fluid narrow className="text-center">
+        <Row>
+          <Col>
+            <LogoTXT big>Count Game Yay!</LogoTXT>
+          </Col>
+        </Row>
+        <SectionWarpper>
+          <Row>
+            <Col>
+              <MaxNumber number={this.props.maxPlayer} title="Player"/>
+            </Col>
+            <Col>
+              <MaxNumber number={this.props.maxNumber} title="Max Number"/>
+            </Col>
+          </Row>
+          <Row>
+            <Col><StyleText>Press Start key to start game</StyleText></Col>
+          </Row>
+        </SectionWarpper>
+      </FullHeightContainer>
+    );
+  }
+}
+
+export default LosePage;
